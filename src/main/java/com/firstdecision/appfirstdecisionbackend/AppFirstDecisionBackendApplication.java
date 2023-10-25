00fc3e359469791c317c72ae173464f4b25b253a
moraes.jonathan <moraes.jonathan@mj.gov.br>
2023-10-25 15:36:14 -0300

    feat - initial commit

---
+++ src/main/java/com/firstdecision/appfirstdecisionbackend/AppFirstDecisionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.firstdecision.appfirstdecisionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFirstDecisionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFirstDecisionBackendApplication.class, args);
+    }
+
+}
